efa86f28a3eb5586c676f88c6a4f472d139e8c78
JFumba <jessefumba@gmail.com>
2019-10-15 10:08:36 +0200

    logic in progress

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,19 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #493840;
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    font-family: 'Avenir', Arial, Helvetica;
+    font-size: 1.6rem;
+    color: #333;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+  }
 </style>
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -44,20 +44,134 @@ export default {
     data: function() {
         return {
             weekdays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+            weekdayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            month: [
+            'January','February','March','April','May','June','July',
+            'August','September','October','November','December'
+            ],
+            currentDate: {
+                date: 0,
+                month: 0,
+                year: 0
+            }
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-    @import url('https://fonts.googleapis.com/css?family=Anton');
-    #calendar {
-        width: 460px;
-        height: 730px;
-        background-color: #efefef;
-        font-family: 'Anton';
-        border-radius: 15px;
-        overflow: hidden;
-        background: url('C:\Users\eight\Desktop\TutoVueJs\webNoobTuto\Calendar\calendar\src\assets\calendar\8.jpg') center center;
+  @import url('https://fonts.googleapis.com/css?family=Anton');
+  @mixin calendar-layout($property) {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
+    grid-gap: 10px;
+    padding: $property;
+    div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 30px;
+      color: #fff;
+      border-radius: 5px;
     }
+  }
+  @mixin arrow-style() {
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    cursor: pointer;
+  }
+  #calendar {
+    width: 460px;
+    height: 730px;
+    background-color: #efefef;
+    font-family: 'Anton';
+    border-radius: 15px;
+    overflow: hidden;
+    background-size: cover;
+    user-select: none;
+    header {
+      display: flex;
+      justify-content: center;
+      align-items: top;
+      height: 400px;
+      padding: 20px 0 0;
+      text-align: center;
+      overflow: hidden;
+      color: #efefef;
+      text-shadow: 1px 1px 1px #222,
+        1px -1px 1px #222,
+        -1px 1px 1px #222,
+        -1px -1px 1px #222;
+      .current-date {
+        width: 300px;
+      }
+      .arrow-up {
+        @include arrow-style();
+        border-bottom: 10px solid #fff;
+        &:hover {
+          border-bottom: 10px solid rgba(0,0,0,.4);
+        }
+      }
+      .arrow-down {
+        @include arrow-style();
+        border-top: 10px solid #fff;
+        &:hover {
+          border-top: 10px solid rgba(0,0,0,.4);
+        }
+      }
+      .today {
+        display: grid;
+        grid-template-columns: 40px auto 70px;
+        grid-gap: 0;
+        div {
+          display: flex;
+          justify-content: center;
+        }
+      }
+      .current-day {
+        font-size: 4rem;
+      }
+      .today {
+        font-size: 2rem;
+      }
+    }
+    .weekdays {
+      @include calendar-layout(10px 20px 10px);
+      background-color: rgba(0,0,0,.5);
+      border-bottom: 1px solid #fff;
+      div:nth-child(7n) {
+        color: #D43541;
+      }
+    }
+    .date {
+      @include calendar-layout(10px 20px 20px);
+      background-color: rgba(0,0,0,.7);
+      .active {
+        background-color: #fff;
+        color: #2A4C50;
+      }
+      .day {
+        cursor: pointer;
+        &:hover {
+          background-color: #fff;
+          color: #2A4C50;
+        }
+        &:nth-child(7n) {
+          color: #D43541;
+          &:hover {
+            background-color: #fff;
+            color: #D43541;
+          }
+        }
+      }
+      .day-hidden {
+        opacity: .4;
+        &:nth-child(7n) {
+          color: #D43541;
+        }
+      }
+    }
+  }
 </style>
