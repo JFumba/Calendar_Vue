fe2913c0d53e8f0c060defa4e8e7c2218d4fd47d
JFumba <jessefumba@gmail.com>
2019-10-15 09:16:25 +0200

    20191015 0916

---
+++ src/components/Calendar.vue
@@ -0,0 +1,62 @@
+<template>
+    <article id="calendar">
+        <header>
+            <div class="current-date">
+                <div class="current-day">
+                    Saturday
+                </div>
+                <div class="today">
+                    <div><div class="arrow-up"></div></div>
+                    <div><div class="arrow-up"></div></div>
+                    <div><div class="arrow-up"></div></div>
+                    <div>10</div>
+                    <div>August</div>
+                    <div>2019</div>
+                    <div><div class="arrow-down"></div></div>
+                    <div><div class="arrow-down"></div></div>
+                    <div><div class="arrow-down"></div></div>
+                </div>
+            </div>
+        </header>
+        <section>
+            <div class="weekdays">
+                <div class="weekday" v-for="(weekday, index) in weekdays" :key="index">
+                    {{ weekday }}
+                </div>
+            </div>
+            <div class="date">
+                <div class="day-hidden">29</div>
+                <div class="day-hidden">30</div>
+                <div class="day-hidden">31</div>
+                <div class="day" v-for="(n, index) in 31" :key="index" >
+                    {{ n }}
+                </div>
+                <div class="day-hidden" v-for="(n, index) in 8" :key="index" >
+                    {{ n }}
+                </div>
+            </div>
+        </section>
+    </article>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            weekdays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import url('https://fonts.googleapis.com/css?family=Anton');
+    #calendar {
+        width: 460px;
+        height: 730px;
+        background-color: #efefef;
+        font-family: 'Anton';
+        border-radius: 15px;
+        overflow: hidden;
+    }
+</style>
